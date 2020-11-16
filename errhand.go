2eac04c376f7c131551107cb09c692d42c6a5ecd
Wojciech Adam Koszek <wojciech@koszek.com>
2020-11-15 18:22:55 -0800

    Use Fatalln instead.

--- errhand.go
+++ errhand.go
@@ -4,10 +4,9 @@ import (
 	"log"
 )
 
-
 // CrashIfErr is expected to crash your program if `err` isn't nill
 func CrashIfErr(err error) {
 	if err != nil {
-		log.Fatal(err, "\n")
+		log.Fatalln(err)
 	}
 }
